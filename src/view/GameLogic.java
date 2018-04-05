d285742332f4d2ca68f38fbd4483d727b1f79489
Francisco Silva <francisjssilva@gmail.com>
2018-04-05 21:12:46 +0100

    edited way to find symmetry and added reduce to 1st quadrant

--- src/view/GameLogic.java
+++ src/view/GameLogic.java
@@ -1,5 +1,7 @@
 package view;
 
+import javafx.util.Pair;
+
 import java.util.ArrayList;
 import java.util.Random;
 
@@ -49,30 +51,177 @@ public class GameLogic {
                     boardArray[12][12] = 1;
 
 
+        //boardArray[11][11] = 2;
+
+        boardArray[1][2] = 2;
+        boardArray[10][1] = 2;
+        boardArray[11][10] = 2;
+        boardArray[2][11] = 2;
+
+        verifyCaptureKing(1,2,2);
+
         printBoard();
+
+        reduceQuadrant(11,11);
+
     }
 
-    boolean verifyCaptureKing(int actualMoveY, int actualMoveX, int player){
-        int newMoveY = actualMoveY - 6;
-        int newMoveX = actualMoveX - 6;
+    public boolean verifyCaptureKing(int currentMoveY, int currentMoveX, int player){
+        int newMoveY = currentMoveY - 6;
+        int newMoveX = currentMoveX - 6;
+        int[][] reducedQuad = new int[13][13];
 
-        for(int i = 0; i < 3; i++){
-            System.out.println(newMoveY + " " +newMoveX);
-            newMoveY = newMoveY * (int)Math.cos(Math.toRadians(180)) + newMoveX * (int)Math.sin(Math.toRadians(180));
-            newMoveX = newMoveX * (int)Math.cos(Math.toRadians(180)) - newMoveY * (int)Math.sin(Math.toRadians(180));
+        for (int i = 0; i < boardArray.length; i++)
+            for (int j = 0; j < boardArray.length; j++)
+                reducedQuad[i][j] = boardArray[i][j];
 
-            System.out.println(newMoveY + " " +newMoveX);
-
-            if(boardArray[newMoveX+6][newMoveY+6] != player)
+        for(int i = 0; i < 4; i++){
+            rotate90Degree(reducedQuad);
+            if(reducedQuad[currentMoveY][currentMoveX] != boardArray[currentMoveY][currentMoveX])
                 return false;
         }
 
-        System.out.println("Existe uma simetria!");
+
+
+        /*for(int i = 0; i < 3; i++){
+            newMoveY = newMoveY * (int)Math.cos(Math.toRadians(90)) + newMoveX * (int)Math.sin(Math.toRadians(90));
+            newMoveX = newMoveX * (int)Math.cos(Math.toRadians(90)) - newMoveY * (int)Math.sin(Math.toRadians(90));
+
+            if(boardArray[newMoveY+6][newMoveX+6] != player)
+                return false;
+        }*/
+
+        System.out.println("Symmetry found!");
 
         return true;
     }
 
-    void printBoard(){
+    public boolean makeMove(int actualY, int actualX, int newY, int newX, int player){
+        return true;
+    }
+
+    public int[][] reduceQuadrant(int currentY, int currentX){
+        int [][] reducedQuad = new int[13][13];
+        int quadrant = solveQuad(currentY, currentX);
+        int alpha = 0;
+
+        if(quadrant == 0){
+            System.out.println("Error! Something went wrong with solveQuad!");
+            System.exit(-1);
+        }
+
+        for (int i = 0; i < boardArray.length; i++)
+            for (int j = 0; j < boardArray.length; j++)
+                reducedQuad[i][j] = boardArray[i][j];
+
+        for(int i = 0; i < quadrant; i++)
+            rotate90Degree(reducedQuad);
+
+        System.out.println();
+
+        for (int i = 0; i < 13; i++){
+            for(int j = 0; j < 13; j++)
+                System.out.print(reducedQuad[i][j] + " ");
+            System.out.println();
+        }
+
+        return reducedQuad;
+    }
+
+    private static void transpose(int[][] m) {
+
+        for (int i = 0; i < m.length; i++) {
+            for (int j = i; j < m[0].length; j++) {
+                int x = m[i][j];
+                m[i][j] = m[j][i];
+                m[j][i] = x;
+            }
+        }
+    }
+
+    private void rotate90Degree(int[][] rotatedQuad) {
+        transpose(rotatedQuad);
+
+        for (int  j = 0; j < rotatedQuad[0].length/2; j++) {
+            for (int i = 0; i < rotatedQuad.length; i++) {
+                int x = rotatedQuad[i][j];
+                rotatedQuad[i][j] = rotatedQuad[i][rotatedQuad[0].length -1 -j];
+                rotatedQuad[i][rotatedQuad[0].length -1 -j] = x;
+            }
+        }
+    }
+
+
+    //sentido anti-horário a começar no canto superior esquerdo
+    private int solveQuad(int y, int x) {
+
+        //excepção dos quadrantes genéricos
+        if(x < 6 && y < 6)
+            return 1;
+        else if(x < 6 && y >6)
+            return 2;
+        else if(x > 6 && y > 6)
+            return 3;
+        else if(x > 6 && y < 6)
+            return 4;
+
+        //excepção para o cruzamento com o rei
+        else if(x == 6 && y < 6)
+            return 1;
+        else if( y == 6 && x < 6)
+            return 2;
+        else if(x == 6 && y > 6)
+            return 3;
+        else if(y == 6 && x > 6)
+            return 4;
+
+        return 0;
+    }
+
+    ArrayList<Pair<Integer,Integer>> possibleMoves(int currentY, int currentX, int player){
+
+        ArrayList<Pair<Integer,Integer>> possibleMoves = new ArrayList<Pair<Integer,Integer>>();
+        int[][] reducedQuadrant = reduceQuadrant(currentY, currentX);
+
+        if(currentX == currentY)
+            for(int i = currentX; i < 5; i++);
+
+        //Verificação na horizontal da posição atual para a frente.
+        /*for(int i = currentX+1; i < boardArray[currentY].length; i++){
+            if(getBoardArray()[currentY][i] == 0)
+                possibleMoves.add(new Pair<Integer, Integer>(currentY, i));
+            else
+                break;
+        }
+
+        //Verificação na horizontal da posição atual para a trás.
+        for(int i = currentX-1; i < boardArray[currentY].length; i--){
+            if(getBoardArray()[currentY][i] == 0)
+                possibleMoves.add(new Pair<Integer, Integer>(currentY, i));
+            else
+                break;
+        }
+
+        //Verificação na vertical da posição atual para baixo.
+        for(int i = currentY+1; i < boardArray.length; i++){
+            if(getBoardArray()[i][currentX] == 0)
+                possibleMoves.add(new Pair<Integer, Integer>(i, currentX));
+            else
+                break;
+        }
+
+        //Verificação na vertical da posição atual para cima.
+        for(int i = currentY-1; i >= 0; i--){
+            if(getBoardArray()[i][currentX] == 0)
+                possibleMoves.add(new Pair<Integer, Integer>(i, currentX));
+            else
+                break;
+        }*/
+
+        return possibleMoves;
+    }
+
+    public void printBoard(){
         for (int i = 0; i < 13; i++){
             for(int j = 0; j < 13; j++)
                 System.out.print(boardArray[i][j] + " ");
